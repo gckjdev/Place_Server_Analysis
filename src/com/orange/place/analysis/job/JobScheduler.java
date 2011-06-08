3fd72c1f90c300d1a2495adfefcb78f0c9331364
rollin.lechen <rollin.lechen@gmail.com>
2011-06-08 18:34:57 +0800

    start status for job execute

--- src/com/orange/place/analysis/job/JobScheduler.java
+++ src/com/orange/place/analysis/job/JobScheduler.java
@@ -1,10 +1,17 @@
 package com.orange.place.analysis.job;
 
+import org.apache.commons.lang.StringUtils;
+
+import com.orange.common.cassandra.CassandraClient;
 import com.orange.common.context.SpringContextUtil;
 
 public class JobScheduler {
 
 	private AnalysisJob analysisJob;
+
+	private CassandraClient cassandraClient;
+
+	private final String RUNNING = "RUNNING";
 
 	public AnalysisJob getAnalysisJob() {
 		if (analysisJob == null) {
@@ -13,10 +20,42 @@ public class JobScheduler {
 		return analysisJob;
 	}
 
-	public void runJob() {
-		// TODO: start control
-		AnalysisJob job = getAnalysisJob();
-		job.execute();
+	public CassandraClient getCassandraClient() {
+		if (cassandraClient == null) {
+			cassandraClient = SpringContextUtil.getBean("cassandraClient");
+		}
+		return cassandraClient;
 	}
 
+	public void runJob() {
+		String status = getStatus();
+		if (!StringUtils.isEmpty(status)) {
+			// don't run the job if previous job is running.
+			return;
+		}
+
+		try {
+			start();
+			AnalysisJob job = getAnalysisJob();
+			job.execute();
+		} finally {
+			end();
+		}
+	}
+
+	private void end() {
+		getCassandraClient().deleteStringColumn("idx_id_long_to_string",
+				"analysisJob_status", "status");
+	}
+
+	private void start() {
+		getCassandraClient().insert("idx_id_long_to_string",
+				"analysisJob_status", "status", RUNNING);
+	}
+
+	private String getStatus() {
+		String status = getCassandraClient().getColumnValue(
+				"idx_id_long_to_string", "analysisJob_status", "status");
+		return status;
+	}
 }
